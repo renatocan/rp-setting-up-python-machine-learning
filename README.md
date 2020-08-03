c90aa52770db21c75af6b9328ae7c72f28aac100
Renato Candido <renatocan@gmail.com>
2020-08-03 19:57:53 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setting Up Python for Machine Learning on Windows
+
+This is the code example used in the article [Setting Up Python for Machine Learning on Windows](https://realpython.com/python-windows-machine-learning-setup/) on Real Python. 
+
+The environment can be set up with conda:
+
+`conda env create -f conda_env.yml`
---
+++ xor.py
@@ -0,0 +1,21 @@
+import numpy as np
+np.random.seed(444)
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.optimizers import SGD
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+model = Sequential()
+model.add(Dense(2, input_dim=2))
+model.add(Activation('sigmoid'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+sgd = SGD(lr=0.1)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+
+model.fit(X, y, batch_size=1, epochs=5000)
+print(model.predict(X))
